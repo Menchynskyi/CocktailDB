d651e702d331a30b197b6ca7fe0438906ebe17d3
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2020-06-09 13:11:34 +0300

    Add eslint and prettier to the project

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text>CoctailDB</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
